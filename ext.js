5f8bf29ddfb5cb1b9176dc89504733a5a3032d59
arahansa <arahansa@naver.com>
2019-10-29 22:54:19 +0900

    외부기능은 자바스크립트로 변경

---
+++ ext.js
@@ -0,0 +1,26 @@
+
+/**
+ *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+ *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+
+};
+*/
+
+var disqus_config = function () {
+    this.page.url = "https://arahansa.github.io/youtube_videolist2csv/";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = 'github_youtube_downloader'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://EXAMPLE.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-57986352-3');
